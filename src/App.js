d95736b145b2071292572612adcc19682e7c16fd
Fabienne Prouteau <faabke@gmail.com>
2018-06-04 14:26:58 +0100

    added limit to search result and filter on rating

--- src/App.js
+++ src/App.js
@@ -10,7 +10,9 @@ class App extends Component {
 
   state = {
     gifs: [],
-    searchfield: ''
+    search: '',
+    rating: 'All',
+    limit: 1 // 1 by default
   };
 
   //ComponentDidMount - ajax call to get gifs from giphy
@@ -24,27 +26,35 @@ class App extends Component {
   }
 
   handleChange = (e) => {
-    this.setState({ searchfield: e.target.value }, () => console.log(this.state.searchfield));
+    const {name, value} = e.target;
+    this.setState({ [name]: value }, () => console.log(this.state));
   }
 
   handleSubmit = (e) => {
     //On submit filter based on input field
     e.preventDefault();
-    axios.get(`http://api.giphy.com/v1/gifs/search?q=${this.state.searchfield}&api_key=74kCblNsHK9mSqTjNIX083FCh6tzBC1u`)
+    axios.get(`http://api.giphy.com/v1/gifs/search?q=${this.state.search}&api_key=74kCblNsHK9mSqTjNIX083FCh6tzBC1u&limit=${this.state.limit}`)
       .then(res => {
-        this.setState({ gifs: res.data.data, searchfield: '' }, () => console.log(this.state));
+        this.setState({ gifs: res.data.data }, () => console.log(this.state));
       });
   }
 
+  //Returns a filtered array of the search results
+  filterRating = () => {
+    if (this.state.rating === 'All') return this.state.gifs;
+    else return this.state.gifs.filter(elt => elt.rating === this.state.rating);
+  }
+
   render() {
+    const filteredArr = this.filterRating();
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Gif and Cie</h1>
         </header>
-        <SearchBar handleChange={this.handleChange} handleSubmit={this.handleSubmit} searchfield={this.state.searchfield} />
+        <SearchBar handleChange={this.handleChange} handleSubmit={this.handleSubmit} handleChangeRating={this.handleChangeRating} />
         <div className="container">
-          <DisplayGifs gifs={this.state.gifs}/>
+          <DisplayGifs gifs={filteredArr}/>
         </div>
       </div>
     );
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,16 +1,29 @@
 import React from 'react';
 
 //Search component - functional component is enough
-const SearchBar = ({ handleChange, handleSubmit, searchfield }) => {
+const SearchBar = ({ handleChange, handleSubmit, handleChangeRating }) => {
   return(
     <div className="searchBar">
       <form onSubmit={handleSubmit}>
         <div className="field">
-          <label htmlFor="search"></label>
-          <input onChange={handleChange} id="search" value={searchfield} className="input" placeholder="Enter search" name="search" />
+          <label htmlFor="search">Search</label>
+          <input onChange={handleChange} id="search" className="input" placeholder="Enter search" name="search" />
+          <label htmlFor="limit">Number of results</label>
+          <input onChange={handleChange} id="limit" className="input" placeholder="Enter number of results - Max 100" name="limit" min="1" max="100" />
         </div>
         <button>Go!</button>
       </form>
+
+      <label htmlFor="rating">Rating</label>
+      <select name="rating" id="rating" onChange={handleChange} defaultValue="All">
+        <option value="All">All</option>
+        <option value="y">y</option>
+        <option value="g">g</option>
+        <option value="pg">pg</option>
+        <option value="pg-13">pg-13</option>
+        <option value="r">r</option>
+      </select>
+
     </div>
   );
 };
